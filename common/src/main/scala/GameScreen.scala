a85ea35d015eeba9d57b88479586dd8df708f069
zephyz zhaar <zephyz.zhaar@gmail.com>
2014-01-25 01:19:17 +0100

    added crazy colors

--- common/src/main/scala/GameScreen.scala
+++ common/src/main/scala/GameScreen.scala
@@ -16,6 +16,7 @@ class GameScreen extends Screen {
   val batch = new SpriteBatch
   val stage = new Stage
   val moveDelta = 8
+  var elapsed = 0f;
   lazy val ship = new Ship(new Texture("art/ship3.png"))
   
   class Ship(text: Texture) extends Image(text){
@@ -54,20 +55,27 @@ class GameScreen extends Screen {
   class obstacle{
     
   }
+  
+  def getRainbow : Color = {
+    def sinValue(e : Float):Float = (Math.sin(elapsed + e).toFloat + 1f)/2f
+    new Color(sinValue(-(Math.PI).toFloat * 2f / 3f), sinValue(Math.PI.toFloat * 2f / 3f), sinValue(0f), 1)
+  }
 
   def render(delta: Float) = {
-    Gdx.gl.glClearColor(0, 0, 0, 1)
+    elapsed += delta
+    val c = getRainbow
+    Gdx.gl.glClearColor(c.r, c.g, c.b , 0.8f)
     Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT)
 
     batch.begin()
 
-    if(Gdx.input.isKeyPressed(Keys.LEFT)) 
-      ship addAction(Actions.moveBy(-moveDelta, 0, 0.2f))
-    if(Gdx.input.isKeyPressed(Keys.RIGHT)) 
-      ship addAction(Actions.moveBy(moveDelta, 0, 0.2f))
-    if(Gdx.input.isKeyPressed(Keys.UP)) 
+    if(Gdx.input.isKeyPressed(Keys.LEFT) || Gdx.input.isKeyPressed(Keys.A)) 
+      ship addAction(Actions.moveBy(-moveDelta, 0, 0.1f))
+    if(Gdx.input.isKeyPressed(Keys.RIGHT) || Gdx.input.isKeyPressed(Keys.D)) 
+      ship addAction(Actions.moveBy(moveDelta, 0, 0.1f))
+    if(Gdx.input.isKeyPressed(Keys.UP) || Gdx.input.isKeyPressed(Keys.W)) 
       ship addAction(Actions.moveBy(0, moveDelta, 0.2f))
-    if(Gdx.input.isKeyPressed(Keys.DOWN)) 
+    if(Gdx.input.isKeyPressed(Keys.DOWN) || Gdx.input.isKeyPressed(Keys.S)) 
       ship addAction(Actions.moveBy(0, -moveDelta, 0.2f))
     stage.act(delta);
     stage.draw;
@@ -82,7 +90,6 @@ class GameScreen extends Screen {
     ship.setX(640);
     ship.setY(360)
     ship.setOrigin(ship.getWidth()/2, ship.getHeight()/2)
-    ship rotate 45
   }
   
   def hide() = { dispose }
