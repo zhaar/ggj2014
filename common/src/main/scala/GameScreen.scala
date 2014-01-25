5af20dfc1ce41d39b037ad39f60844796dda426d
zephyz zhaar <zephyz.zhaar@gmail.com>
2014-01-25 02:28:01 +0100

    added delay on projectiles

--- common/src/main/scala/GameScreen.scala
+++ common/src/main/scala/GameScreen.scala
@@ -18,66 +18,39 @@ class GameScreen extends Screen {
   val stage = new Stage
   val moveDelta = 8
   var elapsed = 0f
+  var delay = 0f
   val maxProjectiles = 4
   lazy val ship = new Ship(new Texture("art/ship3.png"))
-  
-  class Projectile(texture:Texture ) extends Image(texture){
-  }
-  
-  class Ship(text: Texture) extends Image(text){
-    
-    def shoot : Projectile = {
-      val proj = new Projectile(this.getProjectileTexture)
-      proj setPosition(getX() - getOriginX(), getY() - getOriginY())
-      proj addAction(Actions.moveBy(1280, 0, 2))
-      proj
-    }
-    
-    def getProjectileTexture : Texture = new Texture("art/projectile1.png")
-    
-    override def draw(batch: SpriteBatch, alpha: Float): Unit = {
-      validate();
-
-      val color = getColor();
-      batch.setColor(color.r, color.g, color.b, color.a * alpha);
-
-      val x = getX
-      val y = getY
-      val originX = getOriginX
-      val originY = getOriginY
-      val xx = x - originX
-      val yy = y - originY
-      val imageWidth = getWidth
-      val imageHeight = getHeight
-      val scaleX = getScaleX
-      val scaleY = getScaleY
-      val drawable = getDrawable
-
-      val region = drawable.asInstanceOf[TextureRegionDrawable].getRegion();
-      val rotation = getRotation();
-      batch.draw(region,
-          xx,
-          yy,
-          originX,
-          originY,
-          imageWidth,
-          imageHeight,
-          scaleX,
-          scaleY, rotation);
-    }
-  }
-  
-  class obstacle{
-    
-  }
   
   def getRainbow : Color = {
     def sinValue(e : Float):Float = (Math.sin(elapsed + e).toFloat + 1f)/3f
     new Color(sinValue(-(Math.PI).toFloat * 2f / 3f), sinValue(Math.PI.toFloat * 2f / 3f), sinValue(0f), 1)
   }
+  
+  def checkBounds(ship: Ship) : Unit  = {
+    if(ship.getX() < 30) ship.setX(30)
+    if(ship.getX() > 1250) ship setX 1350
+    if(ship.getY() > 690) ship setY 690
+    if(ship.getY() < 30) ship setY 30
+  }
+  
+  def checkScene(scene: Stage): Stage = {
+    val actors = scene.getActors();
+    for(i <- 0 until actors.size){
+      val actor = actors get i
+      actor match{
+        case proj: Projectile => if(actor.getActions().size == 0) actor.remove();
+        case ship: Ship => checkBounds(ship);
+      }
+    }
+    
+    scene
+  }
 
   def render(delta: Float) = {
     elapsed += delta
+    if(delay  > 0) delay -= delta
+    else delay = 0
     val c = getRainbow
     Gdx.gl.glClearColor(c.r, c.g, c.b , 1)
     Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT)
@@ -92,8 +65,11 @@ class GameScreen extends Screen {
       ship addAction(Actions.moveBy(0, moveDelta, 0.2f))
     if(Gdx.input.isKeyPressed(Keys.DOWN) || Gdx.input.isKeyPressed(Keys.S)) 
       ship addAction(Actions.moveBy(0, -moveDelta, 0.2f))
-    if(Gdx.input.isKeyPressed(Keys.SPACE)) 
+    if(Gdx.input.isKeyPressed(Keys.SPACE) && delay == 0){
+      delay = 0.3f;
       stage addActor(ship.shoot)
+    }
+    
 
     stage.act(delta);
     stage.draw;
