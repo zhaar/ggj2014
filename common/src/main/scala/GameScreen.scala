d9a4c3d6311dd2c653ed31a1b6fda927a5f011ed
zephyz zhaar <zephyz.zhaar@gmail.com>
2014-01-25 01:58:45 +0100

    added projectiles

--- common/src/main/scala/GameScreen.scala
+++ common/src/main/scala/GameScreen.scala
@@ -11,15 +11,30 @@ import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable
 import com.badlogic.gdx.scenes.scene2d.actions.Actions
 import com.badlogic.gdx.Input.Keys
+import com.badlogic.gdx.utils.Pool.Poolable
 
 class GameScreen extends Screen {
   val batch = new SpriteBatch
   val stage = new Stage
   val moveDelta = 8
-  var elapsed = 0f;
+  var elapsed = 0f
+  val maxProjectiles = 4
   lazy val ship = new Ship(new Texture("art/ship3.png"))
   
+  class Projectile(texture:Texture ) extends Image(texture){
+  }
+  
   class Ship(text: Texture) extends Image(text){
+    
+    def shoot : Projectile = {
+      val proj = new Projectile(this.getProjectileTexture)
+      proj setPosition(getX() - getOriginX(), getY() - getOriginY())
+      proj addAction(Actions.moveBy(1280, 0, 2))
+      proj
+    }
+    
+    def getProjectileTexture : Texture = new Texture("art/projectile1.png")
+    
     override def draw(batch: SpriteBatch, alpha: Float): Unit = {
       validate();
 
@@ -57,14 +72,14 @@ class GameScreen extends Screen {
   }
   
   def getRainbow : Color = {
-    def sinValue(e : Float):Float = (Math.sin(elapsed + e).toFloat + 1f)/2f
+    def sinValue(e : Float):Float = (Math.sin(elapsed + e).toFloat + 1f)/3f
     new Color(sinValue(-(Math.PI).toFloat * 2f / 3f), sinValue(Math.PI.toFloat * 2f / 3f), sinValue(0f), 1)
   }
 
   def render(delta: Float) = {
     elapsed += delta
     val c = getRainbow
-    Gdx.gl.glClearColor(c.r, c.g, c.b , 0.8f)
+    Gdx.gl.glClearColor(c.r, c.g, c.b , 1)
     Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT)
 
     batch.begin()
@@ -77,6 +92,9 @@ class GameScreen extends Screen {
       ship addAction(Actions.moveBy(0, moveDelta, 0.2f))
     if(Gdx.input.isKeyPressed(Keys.DOWN) || Gdx.input.isKeyPressed(Keys.S)) 
       ship addAction(Actions.moveBy(0, -moveDelta, 0.2f))
+    if(Gdx.input.isKeyPressed(Keys.SPACE)) 
+      stage addActor(ship.shoot)
+
     stage.act(delta);
     stage.draw;
     
