d29527952f47d452878c4d047056f684fa5debdf
zephyz zhaar <zephyz.zhaar@gmail.com>
2014-01-26 12:47:06 +0100

    improved collisisons

--- common/src/main/scala/Bullet.scala
+++ common/src/main/scala/Bullet.scala
@@ -2,13 +2,18 @@ package us.zhaar.ggj
 
 import com.badlogic.gdx.scenes.scene2d.ui.Image
 import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.math.MathUtils
 
 class Bullet(transform : Ship => Unit, texture:Texture) extends Image(texture){
 
   def affectShip(ship: Ship): Unit = transform(ship)
   
-  def contains(x: Float, y: Float): Boolean = 
-    getX() < x && x < getX() + getWidth() && getY() < y && y < getY() + getWidth()
+  def contains(x: Float, y: Float): Boolean = {
+    val yDiff = y - this.getY
+    val xDiff = x - getX
+    val distance = Math.sqrt(xDiff * xDiff + yDiff * yDiff).toFloat
+    distance < 20*getScaleX()/2
+  }
     
   
 }
